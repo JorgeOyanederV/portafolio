6c634ca753832fff54cd8cb4c8abba26030c424a
Jorge Oyaneder <joyaneder1996@gmail.com>
2022-11-29 20:33:48 -0300

    Setting configuration for deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/portafolio",
   siteMetadata: {
     title: `Portafolio`,
     siteUrl: `https://www.yourdomain.tld`
